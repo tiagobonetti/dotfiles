5222377c4b2253075c17979c65cd5771eb356563
Tiago Bonetti <tiago.bonetti@gmail.com>
2018-12-04 16:39:01 +0100

    nvim alias now open files ordered by basename
    they also dont open nvim if no there are no results

--- zsh-custom/alias.zsh
+++ zsh-custom/alias.zsh
@@ -9,8 +9,8 @@ alias ssh='nocorrect ssh'
 
 alias cmake-clang-ninja='CC=clang CXX=clang++ cmake -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
 
-nvim-cpp() { find "$@" -name "*.cpp" -o -name "*.h" | xargs nvim }
-nvim-cmake() { find "$@" -name "CMakeLists.txt" | xargs nvim}
-nvim-files() { find "$@" -type f | xargs nvim }
-nvim-conflicts() { git diff --name-only | uniq | xargs nvim }
+nvim-cpp() { find "$@" \( -name "*.cpp" -o -name "*.h" \) -printf '%f\t%p\n' | sort | cut -f 2 | xargs -r nvim }
+nvim-cmake() { find "$@" -name "CMakeLists.txt" | xargs -r nvim}
+nvim-files() { find "$@" -type f -printf '%f\t%p\n' | sort | cut -f 2 | xargs -r nvim }
+nvim-conflicts() { git diff --name-only | uniq | xargs -r nvim }
 
