50591866dd34129f7eb34376fea724479711791f
Tiago Bonetti <tiago.bonetti@gmail.com>
2019-09-06 17:17:04 +0200

    fzf.zsh: add command to search git history with fzf

--- zsh-custom/fzf.zsh
+++ zsh-custom/fzf.zsh
@@ -1,3 +1,5 @@
 export FZF_DEFAULT_COMMAND='fdfind --type file --color=always'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='--ansi'
+
+alias fzf-commit="git log --pretty=oneline --abbrev-commit --color=always | fzf --preview 'git show {+1} --name-only --color=always' | cut -d' ' -f1"
