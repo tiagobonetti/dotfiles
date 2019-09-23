11a73c9ebdabe1f09e0c10dbc22ada3dd149ea3f
Tiago <tiago.bonetti@gmail.com>
2019-09-23 16:38:31 +0200

    removes the patches mechanism
    It didn't work, is easier to pull changes by hand

--- patches/make-patches.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-DIR="$(dirname $(readlink -f $0))"
-diff -Naur ~/.zshrc.backup $DIR/../.zshrc > $DIR/zshrc.patch
-diff -Naur ~/.oh-my-zsh/themes/agnoster.zsh-theme $DIR/../zsh-custom/themes/agnoster-custom.zsh-theme > $DIR/agnoster.zsh-theme.patch
-
