c3b78a2f72448b79d25b73116faf00388d84ae68
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2020-05-22 09:58:20 +0200

    fixes QT detecting environment

--- zsh-custom/qt.zsh
+++ zsh-custom/qt.zsh
@@ -1,5 +1,6 @@
-QT_LIB=($HOME/Qt/5.<->.<->/gcc_64([-1])(N))
+QT_LIB=($HOME/Qt/5.<->.<->/gcc_64([-1]N))
 QT_BIN=$QT_LIB/bin
+
 if [ -d $QT_BIN ]; then
     export CMAKE_PREFIX_PATH="$QT_LIB:$CMAKE_PREFIX_PATH"
     export PATH="$QT_BIN:$PATH"
