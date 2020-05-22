4fe64b6fc12751de59f5e5883f681955a64e98c8
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2020-05-22 16:52:15 +0200

    install all chromes

--- scripts/install-chrome.sh
+++ scripts/install-chrome.sh
@@ -2,4 +2,4 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install google-chrome-stable google-chrome-beta
+sudo apt-get install google-chrome-stable google-chrome-beta google-chrome-unstable
