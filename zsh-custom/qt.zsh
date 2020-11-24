1f0ec7c65d17a141bad953094b8d8f494e3d059b
Tiago Ferreira Bonetti <tiago.bonetti@gmail.com>
2020-11-24 14:07:12 +0100

    if a had a nickle for every time I update the QT env...

--- zsh-custom/qt.zsh
+++ zsh-custom/qt.zsh
@@ -2,6 +2,10 @@ QT_LIB=(/opt/Qt/5.<->.<->/gcc_64([-1]N))
 QT_BIN=$QT_LIB/bin
 
 if [ -d $QT_BIN ]; then
-    export PATH="$QT_BIN:$PATH"
+    export PATH="$PATH:$QT_BIN"
+    export QT_PLUGIN_PATH="$QT_LIB/plugins"
+    export QT_QPA_PLATFORM_PLUGIN_PATH="$QT_LIB/plugins/platforms"
+    export QML2_IMPORT_PATH="$QT_LIB/qml"
+    export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$QT_LIB"
 fi
 
