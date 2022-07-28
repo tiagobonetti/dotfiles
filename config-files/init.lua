ac48c7ae8dd1ee47a95d6960c655f6a2eb97ca27
Tiago Bonetti <tiago.bonetti@gmail.com>
2022-07-28 16:03:26 +0200

    Add macOS specific link script and config files
    Adds `init.lua` and a Conan profile for macOS

---
+++ config-files/init.lua
@@ -0,0 +1,210 @@
+-- Bootstraps packer on startup if needed
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  --  Small functions
+  use 'tpope/vim-sensible'   -- Defaults
+  use 'tpope/vim-abolish'    -- Better substitute :%S//
+  use 'tpope/vim-abolish'    -- Better substitute :%S//
+  use 'tpope/vim-fugitive'   -- Git wrapper :Gsplit
+  use 'tpope/vim-surround'   -- Change surrounds <l>cs{(
+  use 'tpope/vim-commentary' -- Change surrounds <l>cs{(
+  -- Visuals
+  use 'feline-nvim/feline.nvim' 
+  use 'lewis6991/gitsigns.nvim'
+  use 'iCyMind/NeoSolarized'  -- TODO: review
+  use 'RRethy/vim-illuminate' -- TODO: review
+  use 'luochen1990/rainbow'   -- TODO: review
+  -- Navigation
+  use 'moll/vim-bbye'
+  use 'justinmk/vim-dirvish'
+-- use'junegunn/fzf', { 'do': { -> fzf#install() } }
+-- use 'junegunn/fzf.vim'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0', -- branch = '0.1.x',
+     requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'jremmen/vim-ripgrep'
+  -- Language Server Protocol support
+  use { "williamboman/mason.nvim" }
+-- use 'prabirshrestha/async.vim'
+-- use 'prabirshrestha/vim-lsp'
+-- use 'mattn/vim-lsp-settings'
+  -- Completion
+-- use 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+-- use 'lighttiger2505/deoplete-vim-lsp'
+-- " C++
+  use 'octol/vim-cpp-enhanced-highlight'
+  use 'rhysd/vim-clang-format'
+-- " QML
+  use 'peterhoeg/vim-qml'
+-- "Python
+-- use 'psf/black', { 'branch': 'stable' } 
+
+-- Automatically set up your configuration after cloning packer.nvim
+-- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+-- appearance
+vim.opt.background = "dark"
+vim.cmd([[ colorscheme NeoSolarized ]])
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
+-- behaviour
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.showcmd = true
+vim.opt.hidden = true
+vim.opt.list = true
+vim.opt.errorbells = false
+vim.opt.spelllang = "en"
+vim.opt.clipboard = "unnamedplus"
+
+-- indentation
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- always copy the backup into the file so file-watchers can detect the change
+vim.opt.backupcopy = 'yes'
+
+-- <leader>
+vim.g.mapleader = ' '
+
+-- Makefiles
+vim.cmd([[ autocmd FileType make setlocal noexpandtab ]])
+
+-- Comment with // in c/cpp
+vim.cmd([[ autocmd FileType c,cpp setlocal commentstring=//\ %s ]])
+
+-- Initialize lua plugins
+require('feline').setup()
+require('gitsigns').setup()
+require("mason").setup()
+
+-- Rainbow braces
+vim.g.rainbow_active = '1'
+
+-- Disable lsp
+vim.g.lsp_diagnostics_enabled = '0'
+
+-- Use deoplete.
+-- vim.cmd([[ let g:deoplete#enable_at_startup = 1 ]])
+
+-- Cpp
+vim.g.cpp_class_scope_highlight = '1'
+
+-- Clang format
+-- vim.cmd([[
+-- let g:clang_format#command           = "clang-format"
+-- let g:clang_format#detect_style_file = 1
+-- ]])
+
+-- <c-> move
+vim.cmd([[
+nmap <c-k> :bnext<cr>
+nmap <c-j> :bprev<cr>
+nmap <c-l> :wincmd w<cr>
+nmap <c-h> :wincmd p<cr>
+]])
+
+-- <c-> Plugins
+vim.cmd([[
+nmap <c-f> :ClangFormat<cr>
+nmap <c-s> :SyntasticToggleMode<cr>
+nmap <c-x> :SyntaxToggle<cr>
+]])
+
+-- Tab or Shift-Tab selects the next/preview option
+-- Enter/Esc confirm/cancels completion
+vim.cmd([[
+inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
+inoremap <expr> <CR>    pumvisible() ? "\<C-y>" : "\<CR>"
+inoremap <expr> <Esc>   pumvisible() ? "\<C-e>\<Esc>" : "\<Esc>"
+]])
+
+-- Spelling
+vim.cmd([[ nmap <leader>s :set spell!<cr> ]])
+
+-- Buffers
+vim.cmd([[
+nmap <leader>bc :Bdelete<cr>
+nmap <leader>bd :Bdelete!<cr>
+nmap <leader>bn :enew <cr>
+nmap <leader>bf :Buffers<cr>
+]])
+
+-- Current word commands
+vim.cmd([[
+nmap <leader>wg :Grep <c-r><c-w><cr>
+nmap <leader>ws :%s/\<<c-r><c-w>\>/
+nmap <leader>wS :%S/\<<c-r><c-w>\>/
+]])
+
+-- Buffers + current word commands
+vim.cmd([[
+nmap <leader>bws :bufdo! %s/\<<c-r><c-w>\>/
+nmap <leader>bwS :bufdo! %S/\<<c-r><c-w>\>/
+]])
+
+-- Access and load vimrc
+vim.cmd([[
+nmap <leader>vr :e $MYVIMRC<cr>
+nmap <leader>vs :source $MYVIMRC<cr>
+]])
+
+-- Exit terminal with ESC
+vim.cmd([[ tnoremap <esc> <c-\><c-n> ]])
+
+-- No navigating
+vim.cmd([[
+inoremap <Up>    <nop>
+inoremap <Down>  <nop>
+inoremap <Left>  <nop>
+inoremap <Right> <nop>
+noremap  <Up>    <nop>
+noremap  <Down>  <nop>
+noremap  <Left>  <nop>
+noremap  <Right> <nop>
+]])
+
+-- FZF
+-- vim.cmd([[
+-- nmap <leader>f  :Files<cr>
+-- nmap <leader>ff :Files<cr>
+-- nmap <leader>fg :GFiles<cr>
+-- nmap <leader>fh :Files ~<cr>
+-- nmap <leader>fw :Files ~/lmx/ws<cr>
+-- nmap <leader>/  :Grep 
+-- ]])
+
+-- Ripgrep command with preview
+-- vim.cmd([[
+-- command! -bang -nargs=* Grep
+--   \ call fzf#vim#grep(
+--   \   'rg --column --line-number --no-heading --color=always --smart-case '.shellescape(<q-args>), 1,
+--   \   fzf#vim#with_preview('right:50%', '?'), <bang>0)
+-- ]])
+
+-- Add preview to Files
+-- vim.cmd([[
+-- command! -bang -nargs=? -complete=dir Files
+--   \ call fzf#vim#files(<q-args>, fzf#vim#with_preview('right:50%', '?'), <bang>0)
+-- ]])
---
+++ config-files/link-macos.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import argparse
+
+# Map dotfiles to symlink locations
+CONFIGS = {
+    "zsh": {"dotfile": "zshrc", "symlink": ".zshrc"},
+    "neovim-vim": {"dotfile": "init.vim", "symlink": ".config/nvim/init.vim"},
+    "neovim-lua": {"dotfile": "init.lua", "symlink": ".config/nvim/init.lua"},
+    "spacevim": {"dotfile": "spacevim-init.toml", "symlink": ".SpaceVim.d/init.toml"},
+    "ideavim": {"dotfile": "ideavimrc", "symlink": ".ideavimrc"},
+    "conan-profile": {"dotfile": "conan-profile-macos", "symlink": ".conan/profile/default"},
+}
+
+
+def link(dotfile: str, symlink: str):
+    target = Path.home() / symlink
+
+    if target.is_symlink():
+        print(f"'{target.name}' is a symlink: removing")
+        target.unlink()
+
+    elif target.is_file():
+        backup = target.with_name(target.name + ".bk")
+        print(f"'{target.name}' is a file: making a backup as '{backup}'")
+        target.replace(backup)
+
+    target.parent.mkdir(parents=True, exist_ok=True)
+    source = Path(__file__).resolve().parent / dotfile
+    print(f"Creating link '{target}' to '{source}'")
+    target.symlink_to(source)
+
+
+def unlink(symlink: str, **kwargs):
+    target = Path.home() / symlink
+
+    if not target.is_symlink():
+        print(f"'{target.name}' isn't a link: ignoring")
+        return
+
+    print(f"unlinking '{target.name}'")
+    target.unlink()
+    backup = target.with_name(target.name + ".bk")
+    if backup.is_file():
+        print(f"Restoring backup of'{target.name}'")
+        backup.replace(target)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Link (or unlink) dotfiles.')
+    parser.add_argument('-u', '--unlink', action='store_true',
+                        help='unlink dotfiles and restores backups')
+    parser.add_argument('config', choices=CONFIGS.keys(),
+                        help='Choose config file to (un)link')
+
+    args = parser.parse_args()
+    (unlink if args.unlink else link)(**CONFIGS[args.config])
+
+
+if __name__ == "__main__":
+    main()
