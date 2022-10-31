8215b389462d77b1a12ab1e00b39668bbb72b9e7
Tiago Bonetti <tiago.bonetti@gmail.com>
2022-10-31 10:01:03 +0100

    alias.zsh: Minor clean-up and making som aliases linux only

--- zsh-custom/alias.zsh
+++ zsh-custom/alias.zsh
@@ -2,17 +2,10 @@
 alias l='ls -gAFGh'
 alias gitk='gitk --max-count=500'
 
-# There can be only 3
-alias python='python3'
-
 # avoid unecesary corrections (usually to paths) on this commands
 alias git='nocorrect git'
 alias vim='nocorrect vim'
 alias ssh='nocorrect ssh'
-
-alias cmake-clang-ninja='CC=clang CXX=clang++ cmake -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'
-
-alias fd='fdfind'
 
 nvim-cpp() { find "$@" \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -printf '%f\t%p\n' | sort | cut -f 2 | xargs -r nvim }
 nvim-cmake() { find "$@" -name "CMakeLists.txt" | xargs -r nvim}
@@ -20,3 +13,12 @@ nvim-files() { find "$@" -type f -printf '%f\t%p\n' | sort | cut -f 2 | xargs -r
 nvim-diff() { git diff --name-only | uniq | xargs -r nvim }
 
 alias svim='vim -u ~/SpaceVim/vimrc'
+
+case "$OSTYPE" in
+  linux*)
+    # More ergonomical access to fd on ubuntu
+    alias fd='fdfind'
+  ;;
+esac
+
+
