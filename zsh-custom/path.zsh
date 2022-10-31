197066515d1b293978cd87fb3b2bed1ccd2f6971
Tiago Bonetti <tiago.bonetti@gmail.com>
2022-10-31 10:02:24 +0100

    path.zsh: Move path definitions to custom script and make it OS specific

--- zsh-custom/path.zsh
+++ zsh-custom/path.zsh
@@ -1,3 +1,20 @@
-# append
-path+=("$HOME/.local/bin")
-export PATH
+case "$OSTYPE" in
+  darwin*)
+    # Adds homebrew paths to env
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    # Adds python@3.10 to path
+    export PATH="/opt/homebrew/opt/python@3.10/bin:$PATH"
+    export PATH="/opt/homebrew/opt/python@3.10/libexec/bin:$PATH"
+
+    # For compiling with brew's LLVM on MACOS
+    export LDFLAGS="--ld-path=/opt/homebrew/opt/llvm/bin/ld64.lld"
+    export MACOSX_DEPLOYMENT_TARGET=12.0
+  ;;
+  linux*)
+     path+=("$HOME/.local/bin")
+     export PATH
+  ;;
+esac
+
+
