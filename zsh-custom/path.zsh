aeecaf78b7e6f1f0b5c8eff71558e41a4c5cf523
Tiago Bonetti <tiago.bonetti@gmail.com>
2024-06-14 14:02:27 +0200

    Updates a few version for macOS and fx ideavimrc

--- zsh-custom/path.zsh
+++ zsh-custom/path.zsh
@@ -3,18 +3,24 @@ case "$OSTYPE" in
     # Adds homebrew paths to env
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
-    # Adds python@3.10 to path
-    export PATH="/opt/homebrew/opt/python@3.10/bin:$PATH"
-    export PATH="/opt/homebrew/opt/python@3.10/libexec/bin:$PATH"
+    # Adds clang@18 to path
+    export PATH="/opt/homebrew/opt/llvm@18/bin:$PATH"
+
+    # Adds python@3.12 to path
+    export PATH="/opt/homebrew/opt/python@3.12/bin:$PATH"
+    export PATH="/opt/homebrew/opt/python@3.12/libexec/bin:$PATH"
+
+    # Adds .local/bin
+    export PATH="$HOME/.local/bin:$PATH"
 
     # For compiling with brew's LLVM on MACOS
-    export LDFLAGS="--ld-path=/opt/homebrew/opt/llvm/bin/ld64.lld"
-    export MACOSX_DEPLOYMENT_TARGET=12.0
+    export MACOSX_DEPLOYMENT_TARGET=14.5
   ;;
   linux*)
      path+=("$HOME/.local/bin")
      export PATH
   ;;
 esac
+
 
 
