70b5f662efc136830fdd9eb55d4a821c257f8bc6
Tiago Bonetti <tiago.bonetti@gmail.com>
2025-08-22 14:04:55 +0200

    macOS: Cleaner PATH set-up
    Instead of manually put one Python on path I now used a single `venv`.
    Also I rely on passing compilers directly to build systems so no llvm needed on path.
    Sadly some tools rely on `lld` being on path, so I added just that.

--- zsh-custom/path.zsh
+++ zsh-custom/path.zsh
@@ -3,18 +3,14 @@ case "$OSTYPE" in
     # Adds homebrew paths to env
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
-    # Adds clang@18 to path
-    export PATH="/opt/homebrew/opt/llvm@18/bin:$PATH"
-
-    # Adds python@3.12 to path
-    export PATH="/opt/homebrew/opt/python@3.12/bin:$PATH"
-    export PATH="/opt/homebrew/opt/python@3.12/libexec/bin:$PATH"
-
     # Adds .local/bin
     export PATH="$HOME/.local/bin:$PATH"
 
-    # For compiling with brew's LLVM on MACOS
-    export MACOSX_DEPLOYMENT_TARGET=14.5
+    # Add the main Python virtual environment to the path
+    export PATH="$HOME/.venv/bin:$PATH:"
+
+    # Adds lld to path (some libraries need it available)
+    export PATH="$PATH:/opt/homebrew/opt/lld@20/bin"
   ;;
   linux*)
      path+=("$HOME/.local/bin")
